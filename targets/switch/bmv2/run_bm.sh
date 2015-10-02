218959f036cfccd0168be876351e085e50a84437
Parag Bhide <parag.bhide@barefootnetworks.com>
2015-10-02 11:40:43 -0700

    Enable INT transit on BMv2. - Explicit initialization on bos bits after headers are added - not use mask on selector in parser when using bmv2 (P bit is not checked in vxlan gpe header)

--- targets/switch/bmv2/run_bm.sh
+++ targets/switch/bmv2/run_bm.sh
@@ -1,1 +1,1 @@
-sudo ./switch_bmv2 -i 0@veth0 -i 1@veth2 -i 2@veth4 -i 3@veth6 -i 4@veth8 -i 5@veth10 -i 6@veth12 -i 7@veth14 -i 8@veth16 --thrift-port 10001 --pcap switch_bmv2.json
+sudo ./switch_bmv2 -i 0@veth0 -i 1@veth2 -i 2@veth4 -i 3@veth6 -i 4@veth8 -i 5@veth10 -i 6@veth12 -i 7@veth14 -i 8@veth16 --thrift-port 10001 --pcap $* switch_bmv2.json
