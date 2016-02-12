9dabf0ed0800981269021498035ae3e9f6a1093a
Vladimir Gurevich <vladimir.gurevich@barefootnetworks.com>
2016-02-12 12:00:08 -0800

    Added the missing dependency (libffi-dev), required for successful building of Thrift (for interpreted languages, such as Python)

--- install_deps.sh
+++ install_deps.sh
@@ -21,9 +21,9 @@ sudo apt-get install -y                  \
     libboost-test-dev                    \
     libboost-thread-dev                  \
     libbsd-dev                           \
-    libcffi-dev                          \
     libedit-dev                          \
     libevent-dev                         \
+    libffi-dev                           \
     libfreetype6-dev                     \
     libgmp-dev                           \
     libhiredis-dev                       \
